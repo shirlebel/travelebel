0f6d469d20850e98b8bf95cdc331aef5045fabe3
shirlebel <51803143+shirlebel@users.noreply.github.com>
2026-07-18 14:07:49 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='bali-guide-v2';
+const CACHE='travelebel-v3';
 const CORE=['./','./index.html','./manifest.webmanifest'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
